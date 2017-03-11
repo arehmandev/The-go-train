e0b5e31b204602357f900e5691d4ed2b5935b707
arehmandev <arehmandev@outlook.com>
2017-03-11 19:57:46 +0000

    Println

---
+++ 01-HelloTrain/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Choo choo, welcome to the Go train. Some inspiration:")
+
+	fmt.Println(" 1. Just do it.\n",
+		"2. Embrace the grind.\n",
+		"3. Earn your respect.\n",
+		"4. Think outside the box.\n",
+		"5. Realize your potential.\n",
+		"6. If you believe it you can achieve it.\n",
+		"7. Give everything you've got.\n",
+		"8. Create your own circumstances.\n",
+		"9. Hard work pays off.\n",
+		"10. Take a chance on doing what you love.\n",
+		"11. Embrace mistakes.\n",
+		"12. Have enthusiasm for life.\n",
+		"13. Stay ahead of the game.\n",
+		"14. Failure is acceptable.\n",
+		"15. Be good and stay good.\n",
+		"16. Don't lose your passion.\n",
+		"17. Push your individuality.\n",
+		"18. Always learn more.\n",
+		"19. Utilise life's hardships.\n",
+		"20. Be a problem solver.\n",
+		"21. Your are your greatest investment.\n",
+		"22. Stay true to yourself.\n",
+		"23. Keep pushing.\n",
+		"24. Hold your dream.\n",
+		"25. Be clear and specific in leadership.\n",
+		"26. Prepare yourself for life's opportunities.\n",
+		"27. Ignore the critics.\n",
+		"Credits to a random youtube commenter")
+}
