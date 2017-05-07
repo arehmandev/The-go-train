f0aaab35281dcb1c01802daa1962829b1acd73df
Abdul Rehman <arehmandev@outlook.com>
2017-05-07 14:29:55 +0100

    Raindrops

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -1,8 +1,26 @@
 package raindrops
 
-const testVersion = 3
+import "strconv"
 
-func Convert(int) string
+const (
+	testVersion = 3
+	dropnoise1  = "Pling"
+	dropnoise2  = "Plang"
+	dropnoise3  = "Plong"
+)
 
-// Don't forget the test program has a benchmark too.
-// How fast does your Convert convert?
+func Convert(dropnumber int) (dropnoise string) {
+	if dropnumber%3 == 0 {
+		dropnoise += dropnoise1
+	}
+	if dropnumber%5 == 0 {
+		dropnoise += dropnoise2
+	}
+	if dropnumber%7 == 0 {
+		dropnoise += dropnoise3
+	}
+	if len(dropnoise) == 0 {
+		dropnoise = strconv.Itoa(dropnumber)
+	}
+	return
+}
